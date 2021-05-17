6e1bba4d2bd9a8b7e79c65884085d2adfda72dd1
philippov <bervr@mail.ru>
2021-05-17 20:26:02 +0400

    with ajax

--- geekshop/basketapp/static/basketapp/js/basket.js
+++ geekshop/basketapp/static/basketapp/js/basket.js
@@ -1,18 +1,16 @@
 window.onload = function () {
-    document.getElementById('input [type="number"]').addEventListener('click', () => function () {
-            console.log('1')}
-//     $('.basket_list').on('click', 'input [type="number"] ', function () {
-//         console.log(name)
-//         var t_href = event.target;
-//         $.ajax({
-//             url: "/basket/edit/" + t_href.name + "/" + t_href.value + "/",
-//
-//             success: function (data) {
-//                 $('.basket_list').html(data.result);
-//
-//             },
-//         });
-//
-//         event.preventDefault();
-//     });
-)};
+    $(".basket_list").on("click", 'input[type="number"]', function () {
+        console.log(name)
+        var t_href = event.target;
+        $.ajax({
+            url: "/basket/edit/" + t_href.name + "/" + t_href.value + "/",
+
+            success: function (data) {
+                $('.basket_list').html(data.result);
+
+            },
+        });
+
+        event.preventDefault();
+    })
+};
