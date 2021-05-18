44609879d5d98cf2973d3c7f636a5005ec21683a
bervr <61047637+bervr@users.noreply.github.com>
2021-05-18 07:54:17 +0400

    without ajax (#6)
    * without ajax
    
    * with ajax
    
    * some css fix
    
    Co-authored-by: philippov <bervr@mail.ru>

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404
 from basketapp.models import Basket
+from django.template.loader import render_to_string
+from django.urls import reverse
 from mainapp.models import Product
 # Create your views here.
 
@@ -18,8 +21,11 @@ def basket(request):
 
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
-
+@login_required
 def basket_add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
+
     product = get_object_or_404(Product, pk=pk)
 
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -33,6 +39,32 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
-    content = {}
-    return render(request, 'basketapp/basket.html', content)
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        new_basket_item = Basket.objects.get(pk=int(pk))
+
+        if quantity > 0:
+            new_basket_item.quantity = quantity
+            new_basket_item.save()
+        else:
+            new_basket_item.delete()
+
+        basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+
+        content = {
+            'basket': basket_items,
+        }
+
+        result = render_to_string('inc_basket_list.html', content)
+
+        return JsonResponse({'result': result})
+
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,11 +1,39 @@
+from random import sample
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import ProductCategory, Product
+from basketapp.models import Basket
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+    return sample(list(products), 1)[0]
+
+
+def get_same_products(hot_products):
+    same_products = Product.objects.filter(category=hot_products.category).exclude(pk=hot_products.pk)[:3]
+    return same_products
 
 
 def products(request, pk=None):
     title = 'продукты'
+    category = ''
+    products = ''
+
+    hot_product = get_hot_product()
     categories = ProductCategory.objects.all()
+    basket = get_basket(request.user)
+    same_products = get_same_products(hot_product)
+
+
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by('price')
@@ -19,5 +47,22 @@ def products(request, pk=None):
         'categories': categories,
         'category': category,
         'products': products,
+        'basket': basket,
+        'same_products': same_products,
+        'hot_product': hot_product,
     }
     return render(request, 'products_list.html', context=context)
+
+
+@login_required
+def product(request, pk):
+    title = 'страница продута'
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'title': title,
+        'categories': ProductCategory.objects.all(),
+        'product': product,
+        'basket': get_basket(request.user),
+        'same_products': get_same_products(product),
+    }
+    return render(request, 'product.html', context)
